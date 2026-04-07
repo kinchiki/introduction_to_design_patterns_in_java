b6f89a1ba876aa1d5cbf0587d40e57652c115f1b
Akito Naito <17106187+kinchiki@users.noreply.github.com>
2026-04-07 15:47:53 +0900

    1章の練習問題（ArrayList に変更）

--- src/main/java/ch01/Main.java
+++ src/main/java/ch01/Main.java
@@ -7,6 +7,11 @@ public class Main {
         bookShelf.appendBook(new Book("Bible"));
         bookShelf.appendBook(new Book("Cinderella"));
         bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+        bookShelf.appendBook(new Book("East of Eden"));
+        bookShelf.appendBook(new Book("Frankenstein"));
+        bookShelf.appendBook(new Book("Gulliver's Travels"));
+        bookShelf.appendBook(new Book("Hamlet"));
+
         Iterator it = bookShelf.iterator();
         while (it.hasNext()) {
             Book book = (Book)it.next();
