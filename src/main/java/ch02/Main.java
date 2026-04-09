9b4550455fa0c36709b94cd6926294d5c93d09b0
Akito Naito <17106187+kinchiki@users.noreply.github.com>
2026-04-09 15:06:04 +0900

    2章の継承パターン

---
+++ src/main/java/ch02/Main.java
@@ -0,0 +1,9 @@
+package ch02;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+    }
+}
---
+++ src/main/java/ch02/Print.java
@@ -0,0 +1,6 @@
+package ch02;
+
+public interface Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
